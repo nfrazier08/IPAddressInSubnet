d32021ceedfdcb1f3eebdee10af545875acf967c
neliza <nicolasaelf64@gmail.com>
2019-06-09 17:37:58 -0600

    changed variable scoping

--- subnettingAssignment.js
+++ subnettingAssignment.js
@@ -1,14 +1,21 @@
+
+
+var addressToValidate
+var convertedIPString
+var subnetAddress
+var networkInBinary
+
 //convert unsigned integer to binary
-const unsignedIntegerToBinary = (unsignedInteger) => {
+function unsignedIntegerToBinary (unsignedInteger) {
     for(i = 0, convertedIPString = ''; i < 32; i++){
         convertedIPString +=String(unsignedInteger >>>31), unsignedInteger <<=1    
         }
 
-        return convertedIPString //THIS RETURNS A STRING       
-}  //end
+        return convertedIPString      
+} 
     
 //Log Full Subnet Address
-const logFullSubnetAddress = (subnetMask) => {
+function logFullSubnetAddress (subnetMask){
     let subnetAddressString =''
 
         for(i = 0; i < subnetMask; i++){
@@ -16,56 +23,62 @@ const logFullSubnetAddress = (subnetMask) => {
         } 
 
         while (subnetAddressString.length <= 31){
-            subnetAddressString += 0 
-                
+            subnetAddressString += 0                 
         }  
 
     let subnetAddress = parseFloat(subnetAddressString)
-    console.log(typeof subnetAddress, subnetAddress, "here is subnet address")
+    //console.log(typeof subnetAddress, subnetAddress, "here is subnet address")
     return subnetAddress
 }
 
 //Split and convert CIDR block
-const splitAndConvertCIDRBlock = (cidrBlock) => {
+function splitAndConvertCIDRBlock (cidrBlock)  {
 
     //split CIDR block into the network address and the subnet mask    
     var splitBlock = cidrBlock.split('/')
-    console.log(splitBlock)
+    //console.log(splitBlock)
 
     //NETWORK ADDRESS    
-    let networkAddress = splitBlock[0].split('.')
+    var networkAddress = splitBlock[0].split('.')
         
     //Convert network address to binary
-    let networkString = ''
+    var networkString = ''
 
-    for (let i = 0; i < networkAddress.length; i++) {       
+    for (var i = 0; i < networkAddress.length; i++) {       
         (((networkString += networkAddress[i]) >>> 0).toString(2));                 
     }
 
-    let networkInBinary = parseInt(networkString)
-    
-    console.log(typeof networkInBinary, "network address in number")
+    var networkInBinary = parseInt(networkString)    
+    //console.log(typeof networkInBinary, "network address in number")
     
     //SUBNET MASK
-    let subnetMask = parseInt(splitBlock[1])
-    console.log(typeof subnetMask, subnetMask, "subnet mask in number")
+   var subnetMask = parseInt(splitBlock[1])
+    //console.log(typeof subnetMask, subnetMask, "subnet mask in number")
     
-    logFullSubnetAddress(subnetMask)  
-    
-    return networkString
-} //end
+    logFullSubnetAddress(subnetMask) 
 
-const ipInSubnetComparison = (unsignedInteger, CIDRBock) => {
+    //return networkString
+    return networkInBinary
+} 
+
+
+const ipInSubnetComparison = (unsignedInteger, CIDRBlock) => {
+    //Check for valid IP address
+    
+
     //Call helper functions
-    unsignedIntegerToBinary(unsignedInteger)        
-    splitAndConvertCIDRBlock(CIDRBock)
+    unsignedIntegerToBinary(unsignedInteger);        
+    splitAndConvertCIDRBlock(CIDRBlock);
 
-    //(ipNumber & cidrMask) == cidrAdd_networkaddress
-    (convertedIPString & subnetAddress) == networkInBinary
+    //Comparison for 
+    //((convertedIPString & subnetAddress) == networkInBinary)   
+
+    if (((convertedIPString & subnetAddress) == networkInBinary) === false){
+       console.log("No, " + unsignedInteger + " is not in the subnet " + CIDRBlock)
+    } else if (((convertedIPString & subnetAddress) == networkInBinary) === true){
+       console.log("Yes, " + unsignedInteger + " is in the subnet " + CIDRBlock)
+    }
 }
     
 ipInSubnetComparison(0x62D2ED4B, "98.210.237.192/26")
 
-// var ipNumber = 01100010110100101110110101001011;
-// var cidrMask = 11111111111111111111111111000000;
-// var cidrAdd_networkaddress =  01100010110100101110110111000000;
